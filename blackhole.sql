f77a3901a675af7c1ca10d71be7e22a2fc94ae46
David <david.tkachuk@hotmail.com>
2016-12-20 15:10:54 +0000

    updating database dump

--- blackhole.sql
+++ blackhole.sql
@@ -18,21 +18,32 @@ CREATE TABLE IF NOT EXISTS `tickets` (
   `ticket_id` int(6) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `subject` varchar(64) NOT NULL,
   `author_name` varchar(64) NOT NULL,
+  `author_id` int(11) DEFAULT NULL,
   `category_id` int(11) NOT NULL,
   `date_created` int(11) NOT NULL,
   `date_updated` int(11) NOT NULL,
   `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`ticket_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 CREATE TABLE IF NOT EXISTS `ticket_messages` (
   `message_id` int(11) NOT NULL AUTO_INCREMENT,
   `ticket_id` int(11) NOT NULL,
   `author_name` varchar(64) NOT NULL,
+  `author_id` int(11) DEFAULT NULL,
   `date_created` int(11) NOT NULL,
   `message` text NOT NULL,
   PRIMARY KEY (`message_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(36) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `date_created` int(11) NOT NULL,
+  `rank` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
